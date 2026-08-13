3ae6fd3a54904d61e7e12084a1389e8edfbbb138
Multi-Coder-24 <gtavster77@gmail.com>
2026-08-13 15:29:40 +0100

    Development V2.0.0 Release Candidate 2 Signed-off-by: Multicoder <51382988+multi-coder-24@users.noreply.github.com>

--- src/main/java/org/multicoder/mcpaintball/network/TeamSelectC2SPacket.java
+++ src/main/java/org/multicoder/mcpaintball/network/TeamSelectC2SPacket.java
@@ -7,6 +7,7 @@ import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.Identifier;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
+import net.neoforged.fml.ModList;
 import net.neoforged.neoforge.network.PacketDistributor;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 import org.jspecify.annotations.NonNull;
@@ -14,6 +15,7 @@ import org.multicoder.mcpaintball.MCPaintball;
 import org.multicoder.mcpaintball.core.MCPaintballDataAttachments;
 import org.multicoder.mcpaintball.data.MCPaintballPlayerData;
 import org.multicoder.mcpaintball.event.MCPaintballGameEvents;
+import org.multicoder.mcpaintball.integration.MCPaintballVoiceChatPlugin;
 import org.multicoder.mcpaintball.integration.MinecraftTeamSystem;
 
 import java.util.Objects;
@@ -40,12 +42,42 @@ public record TeamSelectC2SPacket(int Team) implements CustomPacketPayload {
             data.Team = Team;
             player.setData(MCPaintballDataAttachments.PAINTBALL_PLAYER,data);
             switch (Team) {
-                case 1 -> server.getScoreboard().addPlayerToTeam(player.getName().getString(),MinecraftTeamSystem.RED);
-                case 2 -> server.getScoreboard().addPlayerToTeam(player.getName().getString(),MinecraftTeamSystem.GREEN);
-                case 3 -> server.getScoreboard().addPlayerToTeam(player.getName().getString(),MinecraftTeamSystem.BLUE);
-                case 4 -> server.getScoreboard().addPlayerToTeam(player.getName().getString(),MinecraftTeamSystem.YELLOW);
-                case 5 -> server.getScoreboard().addPlayerToTeam(player.getName().getString(),MinecraftTeamSystem.PINK);
-                case 6 -> server.getScoreboard().addPlayerToTeam(player.getName().getString(),MinecraftTeamSystem.ORANGE);
+                case 1 -> {
+                    server.getScoreboard().addPlayerToTeam(player.getName().getString(),MinecraftTeamSystem.RED);
+                    if(ModList.get().isLoaded("voicechat")){
+                        Objects.requireNonNull(MCPaintballVoiceChatPlugin.SERVER.getConnectionOf(player.getUUID())).setGroup(MCPaintballVoiceChatPlugin.RED);
+                    }
+                }
+                case 2 -> {
+                    server.getScoreboard().addPlayerToTeam(player.getName().getString(),MinecraftTeamSystem.GREEN);
+                    if(ModList.get().isLoaded("voicechat")){
+                        Objects.requireNonNull(MCPaintballVoiceChatPlugin.SERVER.getConnectionOf(player.getUUID())).setGroup(MCPaintballVoiceChatPlugin.GREEN);
+                    }
+                }
+                case 3 -> {
+                    server.getScoreboard().addPlayerToTeam(player.getName().getString(),MinecraftTeamSystem.BLUE);
+                    if(ModList.get().isLoaded("voicechat")){
+                        Objects.requireNonNull(MCPaintballVoiceChatPlugin.SERVER.getConnectionOf(player.getUUID())).setGroup(MCPaintballVoiceChatPlugin.BLUE);
+                    }
+                }
+                case 4 -> {
+                    server.getScoreboard().addPlayerToTeam(player.getName().getString(),MinecraftTeamSystem.YELLOW);
+                    if(ModList.get().isLoaded("voicechat")){
+                        Objects.requireNonNull(MCPaintballVoiceChatPlugin.SERVER.getConnectionOf(player.getUUID())).setGroup(MCPaintballVoiceChatPlugin.YELLOW);
+                    }
+                }
+                case 5 -> {
+                    server.getScoreboard().addPlayerToTeam(player.getName().getString(),MinecraftTeamSystem.PINK);
+                    if(ModList.get().isLoaded("voicechat")){
+                        Objects.requireNonNull(MCPaintballVoiceChatPlugin.SERVER.getConnectionOf(player.getUUID())).setGroup(MCPaintballVoiceChatPlugin.PINK);
+                    }
+                }
+                case 6 -> {
+                    server.getScoreboard().addPlayerToTeam(player.getName().getString(),MinecraftTeamSystem.ORANGE);
+                    if(ModList.get().isLoaded("voicechat")){
+                        Objects.requireNonNull(MCPaintballVoiceChatPlugin.SERVER.getConnectionOf(player.getUUID())).setGroup(MCPaintballVoiceChatPlugin.ORANGE);
+                    }
+                }
             }
             PacketDistributor.sendToPlayer(player,new DataSyncS2CPacket(player.getData(MCPaintballDataAttachments.PAINTBALL_PLAYER)));
         }
